ed8c10a4016cde67d45d21ce0d1dc789e8e58e6e
fedilayoub <ayoubzahi2016@gmail.com>
2023-07-14 23:30:37 +0100

    added recommandtion and advantages to the LP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Hero from "./components/hero";
+import Recommandation from "./components/recommandation";
+import Advantages from "./components/advantages";
 function App() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Recommandation />
+      <Advantages />
+    </>
+  );
 }
 
 export default App;
