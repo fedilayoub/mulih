bf154f16a7d9b0a1f7fa1a0690edf7bf440cc496
fedilayoub <ayoubzahi2016@gmail.com>
2023-07-16 20:25:24 +0100

    added remaining sections to LP

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Hero from "./components/hero";
 import Recommandation from "./components/recommandation";
 import Advantages from "./components/advantages";
+import CTA from "./components/cta";
+import Testimonials from "./components/testimonials";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
       <Hero />
       <Recommandation />
       <Advantages />
+      <CTA />
+      <Testimonials />
+      <Footer />
     </>
   );
 }
